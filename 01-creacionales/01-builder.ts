d8e7cf74c63d1515ea5a78a2f0f878f3b659063a
Stefan <stefansandorovssb@gmail.com>
2025-04-22 16:49:05 +0200

    terminado el patron builder

--- 01-creacionales/01-builder.ts
+++ 01-creacionales/01-builder.ts
@@ -12,3 +12,64 @@
  *
  * https://refactoring.guru/es/design-patterns/builder
  */
+
+import { COLORS } from "../helpers/colors.ts";
+
+class Computer {
+  public cpu: string = "cpu - not defined";
+  public ram: string = "ram - not defined";
+  public storage: string = "storage - not defined";
+  public gpu?: string;
+
+  displayConfiguration() {
+    console.log(`Configuracion del ordenador
+        CPU: ${this.cpu}
+        RAM: ${this.ram}
+        STORAGE: ${this.storage}
+        GPU: ${this.gpu ?? "No tiene GPU"}`);
+  }
+}
+
+class ComputerBuilder {
+  private computer: Computer;
+
+  constructor() {
+    this.computer = new Computer();
+  }
+
+  setCPU(cpu: string): ComputerBuilder {
+    this.computer.cpu = cpu;
+    return this;
+  }
+
+  setRam(ram: string): ComputerBuilder {
+    this.computer.ram = ram;
+    return this;
+  }
+
+  setStorage(storage: string): ComputerBuilder {
+    this.computer.storage = storage;
+    return this;
+  }
+  setGPU(gpu: string): ComputerBuilder {
+    this.computer.gpu = gpu;
+    return this;
+  }
+
+  build() {
+    return this.computer;
+  }
+}
+
+function main() {
+  const basicComputer = new ComputerBuilder()
+    .setCPU("Intel Core 2 Duo")
+    .setRam("4GB")
+    .setStorage("256GB")
+    .build();
+
+  console.log("%cComputadora basica:", COLORS.blue);
+  basicComputer.displayConfiguration();
+}
+
+main();
