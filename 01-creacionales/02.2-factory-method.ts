d05cd900fe4597146c10646c890897552ba26495
placeholder <placeholder>
2025-04-28 17:23:16 +0200

    finished factory method pattern

--- 01-creacionales/02.2-factory-method.ts
+++ 01-creacionales/02.2-factory-method.ts
@@ -37,17 +37,23 @@ interface Report {
 class SalesReport implements Report {
   // TODO: implementar el método e imprimir en consola:
   // 'Generando reporte de ventas...'
+  generate(): void {
+    console.log('Generando reporte de %cventas...', COLORS.green);
+  }
 }
 
 class InventoryReport implements Report {
   // TODO: implementar el método e imprimir en consola:
   // 'Generando reporte de inventario...'
+  generate(): void {
+    console.log('Generando reporte de %cinventario...', COLORS.yellow);
+  }
 }
 
 // 3. Clase Base ReportFactory con el Método Factory
 
 abstract class ReportFactory {
-  abstract createReport(): Report;
+  protected abstract createReport(): Report;
 
   generateReport(): void {
     const report = this.createReport();
@@ -59,13 +65,13 @@ abstract class ReportFactory {
 
 class SalesReportFactory extends ReportFactory {
   createReport(): Report {
-    throw new Error("Method not implemented.");
+    return new SalesReport();
   }
 }
 
 class InventoryReportFactory extends ReportFactory {
   createReport(): Report {
-    throw new Error("Method not implemented.");
+    return new InventoryReport();
   }
 }
 
@@ -75,9 +81,7 @@ function main() {
   let reportFactory: ReportFactory;
 
   const reportType = prompt(
-    "¿Qué tipo de reporte deseas? %c(sales/inventory)",
-    COLORS.red
-  );
+    "¿Qué tipo de reporte deseas? %c(sales/inventory)");
 
   if (reportType === "sales") {
     reportFactory = new SalesReportFactory();
