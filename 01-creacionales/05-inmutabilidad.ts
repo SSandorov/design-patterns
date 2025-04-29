0798b831e8499c0e39f86e3a28958036dd7fa567
Stefan <stefansandorovssb@gmail.com>
2025-04-29 17:28:47 +0200

    working on immutability

--- 01-creacionales/05-inmutabilidad.ts
+++ 01-creacionales/05-inmutabilidad.ts
@@ -6,3 +6,62 @@
  *  * Es útil para mantener un historial de estados en aplicaciones interactivas.
  *
  */
+
+import { COLORS } from "../helpers/colors.ts";
+
+class CodeEditorState {
+  //   readonly content: string;
+  //   readonly cursorPosition: number;
+  //   readonly unsavedChanges: boolean;
+
+  constructor(
+    readonly content: string,
+    readonly cursorPosition: number,
+    readonly unsavedChanges: boolean
+  ) {}
+
+  //* El partial hace todas las propiedades de la clase opcionales
+  copyWith({
+    content,
+    cursorPosition,
+    unsavedChanges,
+  }: Partial<CodeEditorState>): CodeEditorState {
+    return new CodeEditorState(
+      content ?? this.content,
+      cursorPosition ?? this.cursorPosition,
+      unsavedChanges ?? this.unsavedChanges
+    );
+  }
+
+  displayState() {
+    console.log("\n%cEstado del editor", COLORS.green);
+
+    console.log(`
+            Contenido: ${this.content}
+            Cursor Pos: ${this.cursorPosition}
+            Unsaved Changes: ${this.unsavedChanges}
+            `);
+  }
+}
+
+class CodeEditorHistory {
+  private history: CodeEditorState[] = [];
+  private currentIndex: number = -1;
+
+  save(state: CodeEditorState): void {
+    if (this.currentIndex < this.history.length - 1) {
+      this.history = this.history.splice(0, this.currentIndex + 1);
+    }
+
+    this.history.push(state);
+    this.currentIndex++;
+  }
+
+  redo(): CodeEditorState | null {
+    if (this.currentIndex < this.history.length - 1) {
+      this.currentIndex++;
+      return this.history[this.currentIndex];
+    }
+    return null;
+  }
+}
