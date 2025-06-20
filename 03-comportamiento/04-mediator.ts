939d985c2d9baf7d4fc70610f797ed07931eb606
Stefan <stefansandorovssb@gmail.com>
2025-06-20 17:29:45 +0200

    fininshed mediator pattern

--- 03-comportamiento/04-mediator.ts
+++ 03-comportamiento/04-mediator.ts
@@ -1,3 +1,4 @@
+import { COLORS } from "../helpers/colors.ts";
 /**
  * ! Patrón mediator
  * Es un patrón de diseño de comportamiento que ayuda a reducir
@@ -9,3 +10,68 @@
  *
  * https://refactoring.guru/es/design-patterns/mediator
  */
+
+class User {
+  private username: string;
+  private chatRoom: Chatroom;
+
+  constructor(username: string, chatRoom: Chatroom) {
+    this.username = username;
+    this.chatRoom = chatRoom;
+
+    chatRoom.addUser(this);
+  }
+
+  sendMessage(message: string): void {
+    console.log(
+      `\n\n%c${this.username} envia: %c${message}`,
+      COLORS.blue,
+      COLORS.white
+    );
+    this.chatRoom.sendMessage(this, message);
+  }
+  receiveMessage(sender: User, message: string): void {
+    console.log(
+      `%c${this.username} recibe de ${sender.username}: %c${message}`,
+      COLORS.blue,
+      COLORS.white
+    );
+  }
+}
+class Chatroom {
+  private users: User[] = [];
+  public title: string;
+
+  constructor(title: string) {
+    this.title = title;
+  }
+
+  addUser(user: User) {
+    this.users.push(user);
+  }
+
+  sendMessage(sender: User, message: string): void {
+    const usersToSend = this.users.filter((user) => user !== sender);
+    for (const user of usersToSend) {
+      if (user !== sender) {
+        user.receiveMessage(sender, message);
+      }
+    }
+  }
+}
+
+function main() {
+  const chatRoom = new Chatroom("grupo de trabajo");
+
+  const user1 = new User("Fernando", chatRoom);
+  const user2 = new User("Gaston", chatRoom);
+  const user3 = new User("Miguel", chatRoom);
+
+  user1.sendMessage("Hola a todos");
+  user2.sendMessage("Hola Fernando, como estas?");
+  user3.sendMessage("Buenas! como estan?");
+
+  console.log("\n\n");
+}
+
+main();
--- 03-comportamiento/04.2-mediator.ts
+++ 03-comportamiento/04.2-mediator.ts
@@ -29,19 +29,27 @@
     notificándoles de la actividad de cada avión.
  */
 
-import { COLORS } from '../helpers/colors.ts';
+import { COLORS } from "../helpers/colors.ts";
 
 // Clase Mediador - ControlTower
 class ControlTower {
   private airplanes: Airplane[] = [];
 
   // Registrar un avión en la torre de control
-  // TODO: Implementar el método registerAirplane
-  // registerAirplane(airplane: Airplane)
+  registerAirplane(airplane: Airplane): void {
+    this.airplanes.push(airplane);
+  }
 
   // Enviar un mensaje de un avión a todos los demás
-  //TODO: Implementar el método sendMessage
-  // sendMessage(sender: Airplane, message: string): void
+  sendMessage(sender: Airplane, message: string): void {
+    const notifiedAirplanes = this.airplanes.filter(
+      (airplane) => airplane !== sender
+    );
+
+    for (const airplane of notifiedAirplanes) {
+      airplane.receiveMessage(sender, message);
+    }
+  }
 
   // Coordinación de aterrizaje
   requestLanding(sender: Airplane): void {
@@ -75,7 +83,7 @@ class Airplane {
     this.id = id;
     this.controlTower = controlTower;
 
-    // TODO: Registrar el avión en la torre de control
+    controlTower.registerAirplane(this);
   }
 
   getId(): string {
@@ -86,14 +94,14 @@ class Airplane {
   requestLanding(): void {
     console.log(`${this.id} solicita permiso para aterrizar.`);
 
-    // TODO: Solicitar aterrizaje a la torre de control
+    this.controlTower.requestLanding(this);
   }
 
   // Solicitar despegue a la torre de control
   requestTakeoff(): void {
     console.log(`${this.id} solicita permiso para despegar.`);
 
-    // TODO: Solicitar despegue a la torre de control
+    this.controlTower.requestTakeoff(this);
   }
 
   // Recibir mensaje de otros aviones
@@ -110,9 +118,9 @@ class Airplane {
 function main(): void {
   const controlTower = new ControlTower();
 
-  const airplane1 = new Airplane('Vuelo 101', controlTower);
-  const airplane2 = new Airplane('Vuelo 202', controlTower);
-  const airplane3 = new Airplane('Vuelo 303', controlTower);
+  const airplane1 = new Airplane("Vuelo 101", controlTower);
+  const airplane2 = new Airplane("Vuelo 202", controlTower);
+  const airplane3 = new Airplane("Vuelo 303", controlTower);
 
   // Ejemplo de interacciones
   airplane1.requestLanding();
