b4082be6152c97adb0a7923841eecae2787baec2
Stefan <stefansandorovssb@gmail.com>
2025-06-21 14:26:56 +0200

    finished observer pattern

--- 03-comportamiento/06-observer.ts
+++ 03-comportamiento/06-observer.ts
@@ -13,3 +13,78 @@
  *
  * https://refactoring.guru/es/design-patterns/observer
  */
+
+import { COLORS } from "../helpers/colors.ts";
+
+interface Observer {
+  notify(videoTitle: string): void;
+}
+
+class YoutubeChannel {
+  private subscribers: Observer[] = [];
+  private name: string;
+
+  constructor(name: string) {
+    this.name = name;
+  }
+
+  subscribe(observer: Observer): void {
+    this.subscribers.push(observer);
+    console.log(`Nuevo suscriptor al canal: %c${this.name}`, COLORS.green);
+  }
+
+  unsubscribe(observer: Observer): void {
+    this.subscribers = this.subscribers.filter((sub) => sub !== observer);
+    console.log(`Un suscriptor se ha dado de baja en el canal ${this.name}`);
+  }
+
+  uploadVideo(videoTitle: string): void {
+    console.log(
+      `Canal ${this.name} ha subido un nuevo video: %c${videoTitle}`,
+      COLORS.blue
+    );
+    this.subscribers.forEach((sub) => sub.notify(videoTitle));
+  }
+}
+
+class Subscriber implements Observer {
+  private name: string;
+
+  constructor(name: string) {
+    this.name = name;
+  }
+  notify(videoTitle: string): void {
+    console.log(
+      `%c${this.name} %cha sido notificado: Nuevo video: %c${videoTitle}`,
+      COLORS.red,
+      COLORS.white,
+      COLORS.yellow
+    );
+  }
+}
+
+function main() {
+  const channel = new YoutubeChannel("Cocinando con Fernando");
+
+  const melissa = new Subscriber("Melissa");
+  const cesar = new Subscriber("Cesar");
+  const emilio = new Subscriber("Emilio");
+
+  channel.subscribe(melissa);
+  channel.subscribe(cesar);
+
+  channel.uploadVideo("Receta de tamales de Angular");
+
+  channel.subscribe(emilio);
+
+  channel.uploadVideo("Receta de React al pastor");
+
+  channel.unsubscribe(cesar);
+
+  channel.uploadVideo("Receta de Vue de choplo");
+  channel.unsubscribe(emilio);
+
+  console.log("\n\n");
+}
+
+main();
